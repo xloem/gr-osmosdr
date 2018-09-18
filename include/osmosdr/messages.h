2e31a1efa7491bdf2d7f3c0af76b7effdc74e2bc
Karl Semich <0xloem@gmail.com>
2018-09-18 00:20:37 +0000

    add support for message commands to all devices
    refactored just a little to provide for this

--- include/osmosdr/messages.h
+++ include/osmosdr/messages.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2012 Dimitri Stolnikov <horiz0n@gmx.net>
+ * Copyright 2018 Karl Semich <0xloem@gmail.com>
  *
  * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,22 +17,21 @@
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
+#ifndef INCLUDED_OSMOSDR_MESSAGES_H
+#define INCLUDED_OSMOSDR_MESSAGES_H
 
-#ifndef OSMOSDR_SINK_IFACE_H
-#define OSMOSDR_SINK_IFACE_H
+#include <pmt/pmt.h>
 
-#include <osmosdr/ranges.h>
-#include <osmosdr/time_spec.h>
-#include <gnuradio/basic_block.h>
+namespace osmosdr {
 
-#include "common_iface.h"
+const pmt::pmt_t CMD_PORT = pmt::mp("command");
+const pmt::pmt_t CMD_CHAN_KEY = pmt::mp("chan");
+const pmt::pmt_t CMD_GAIN_KEY = pmt::mp("gain");
+const pmt::pmt_t CMD_FREQ_KEY = pmt::mp("freq");
+const pmt::pmt_t CMD_RATE_KEY = pmt::mp("rate");
+const pmt::pmt_t CMD_ANTENNA_KEY = pmt::mp("antenna");
+const pmt::pmt_t CMD_BANDWIDTH_KEY = pmt::mp("bandwidth");
 
-/*!
- * TODO: document
- *
- */
-class sink_iface : public common_iface
-{
-};
+} /* namespace osmosdr */
 
-#endif // OSMOSDR_SINK_IFACE_H
+#endif /* INCLUDED_OSMOSDR_MESSAGES_H */
